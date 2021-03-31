635feac8d53f1116da8de1779f6203ef5bb571eb
Darina <darina00128@gmail.com>
2021-03-31 10:55:56 +0300

    lab4

---
+++ app/src/main/java/a/e/recyclerview/MainActivity.java
@@ -0,0 +1,21 @@
+package a.e.recyclerview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        recyclerView = findViewById(R.id.recyclerView);
+
+        RViewAdapter rViewAdapter = new RViewAdapter();
+        recyclerView.setAdapter(RViewAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(contex ));
+    }
+}
