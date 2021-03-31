5282949e522a44f96c3ca5ebb79d6b4a1bf47b41
Darina <darina00128@gmail.com>
2021-03-31 14:30:49 +0300

    lab4

--- app/src/main/java/a/e/recyclerview/DetailActivity.java
+++ app/src/main/java/a/e/recyclerview/DetailActivity.java
@@ -1,14 +1,42 @@
 package a.e.recyclerview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DetailActivity extends AppCompatActivity {
+    TextView details;
+    RViewAdapter rViewAdapter = new RViewAdapter(DummyContent.ITEMS);
 
+    public static final List<DummyContent.DummyItem> ITEMS = new ArrayList<DummyContent.DummyItem>();
+    private static final int COUNT = 25;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        details = findViewById(R.id.ARG_ITEM_ID);
+
+        for (int k = 1; k <= COUNT; k++)  {
+            StringBuilder builder = new StringBuilder();
+            builder.append("Информация об элементе:   ").append(k);
+            //details.setText(k);
+            for (int j = 0; j < k; j++) {
+                builder.append("\n Детальная информация. ");
+            }
+
+            ITEMS.add(new DummyContent.DummyItem(String.valueOf(k), "Элемент " + k, builder.toString()));
+            details.setText(builder);
+        }
+
+
+
+
     }
 }
--- app/src/main/java/a/e/recyclerview/MainActivity.java
+++ app/src/main/java/a/e/recyclerview/MainActivity.java
@@ -5,17 +5,20 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     RecyclerView recyclerView;
+    RViewAdapter rViewAdapter = new RViewAdapter(DummyContent.ITEMS);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView = findViewById(R.id.list);
 
-        RViewAdapter rViewAdapter = new RViewAdapter();
-        recyclerView.setAdapter(RViewAdapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager(contex ));
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(rViewAdapter);
+
+
     }
 }
